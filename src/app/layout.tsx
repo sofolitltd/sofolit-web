863d18fe4cee0a4087e3f360efd1ada92ff1d3da
sofol it <sofolitltd@gmail.com>
2026-03-08 10:14:02 +0000

    add top loader like youtube while npage need load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { Header } from '@/components/Header';
 import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata: Metadata = {
   title: 'Sofol IT | Idea to Product for Solo Founders',
@@ -22,6 +24,17 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background text-foreground min-h-screen">
+        <NextTopLoader 
+          color="hsl(220, 80%, 50%)"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px hsl(220, 80%, 50%),0 0 5px hsl(220, 80%, 50%)"
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
