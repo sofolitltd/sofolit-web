7660d1eaa182c3e88a1f2c115957e017d7777dad
sofol it <sofolitltd@gmail.com>
2026-03-10 10:02:18 +0000

    make 35 blogs . topic like.
    domain. hosting
    firebase,
    firebase crud,
    fc

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,11 @@
+
 "use client";
 
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Footer } from "@/components/sections/Footer";
-import { ExternalLink, ArrowRight, Star, Calendar } from "lucide-react";
+import { ArrowRight, Calendar } from "lucide-react";
 import { projectsData } from "@/lib/projects-data";
 
 export default function ProjectsPage() {
